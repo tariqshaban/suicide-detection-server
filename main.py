27981ec06aaac7766cbf44e12d1067058615581b
G.Tata <tariqshaban1999@yahoo.com>
2023-08-08 16:35:34 +0300

    Added 'sample' endpoint in main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
 from transformers import pipeline
+import pandas as pd
 
 MODEL_PATH = './assets/model'
+
+df = pd.read_csv('./assets/dataset/Suicide_Detection.csv')
 
 trained_pipeline = pipeline('sentiment-analysis', model=MODEL_PATH, tokenizer=MODEL_PATH)
 
@@ -21,6 +24,16 @@ def predict():
     prediction = trained_pipeline(text, truncation=True, max_length=4096)[0]
 
     response = jsonify(prediction)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.status = 200
+    return response
+
+
+@app.route('/sample', methods=['GET'])
+def sample():
+    sample_instance = df.sample(n=1)['text'].item()
+
+    response = jsonify({'text': sample_instance})
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.status = 200
     return response
